c4204fcdfa880d16e0fc4f0ff3514a46ed5e88a6
MeanPaper <66984448+MeanPaper@users.noreply.github.com>
2022-06-14 00:20:04 -0500

    make it look a bit nicer

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import React_Icon_Large from '../images/react-icon-large.png';
 
 const Main = () => {
     return (
     <div className = 'content'>
         <h1>Fun facts about React</h1>
-        <ul>
+        <ul className = 'content-unordered-list'>
             <li><span>Was first released in 2013</span></li>
             <li><span>Was originally created by Jordan Walke</span></li>
             <li><span>Has well over 100K stars on GitHub</span></li>
             <li><span>Is maintained by Facebook</span></li>
             <li><span>Power thousands of enterprise apps, including mobile apps</span></li>
         </ul>
+        <img className = 'content-img' src = {React_Icon_Large} alt = 'large semi react icon' />
     </div>);
 };
 
